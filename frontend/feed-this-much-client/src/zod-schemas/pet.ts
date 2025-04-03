e561f9ee256bcd9668a6fef0ba49e31edabca580
Alicia Emily Van Zijl <aevz@kth.se>
2025-04-03 09:27:15 +0200

    Create date of birth input on pet form

--- frontend/feed-this-much-client/src/zod-schemas/pet.ts
+++ frontend/feed-this-much-client/src/zod-schemas/pet.ts
@@ -2,9 +2,8 @@
 import { z } from "zod";
 
 export const addPetSchema = z.object({
-    petname: z.string().min(2, {
-      message: "Pet name must be at least 2 characters.",
-    }),
+    petname: z.string().min(2, "Pet name must be at least 2 characters."),
+    dateOfBirth: z.string().date("Please enter a date in the format YYYY-MM-DD"),
 })
 
 export type addPetSchemaType = typeof addPetSchema._type
