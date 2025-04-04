86814717e448c7415969d34633ecf820e661fc79
Erik Sevelin <91209290+SortHvit@users.noreply.github.com>
2025-04-04 13:49:30 +0200

    define functions for feeding

--- frontend/feed-this-much-client/src/app/calculations.js
+++ frontend/feed-this-much-client/src/app/calculations.js
@@ -1,39 +1,43 @@
 
 //bodyWeight: kg
-//-DailyEnergy: kJ
+//DailyEnergy: kJ
 
-//cats
-
-const neuteredOrIndoor = false
-const catAgeMonths = undefined
-let catEnergyBase
-let catLifeStageFactor
-let catWeight
-if (catAgeMonths < 12) {
-    catEnergyBase  = 314
-    if (catAgeMonths < 4) {
-        catLifeStageFactor = 2.5
-    } else if (catAgeMonths < 9) {
-        catLifeStageFactor = 2
+function calculateCatFeeding(ageMonths, neuteredOrIndoor, weight) { //neuteredOrIndoor: 1 if true, 0 if false
+    let catEnergyBase
+    let catLifeStageFactor
+    if (ageMonths < 12) {
+        catEnergyBase  = 314
+        if (ageMonths < 4) {
+            catLifeStageFactor = 2.5
+        } else if (catAgeMonths < 9) {
+            catLifeStageFactor = 2
+        } else {
+            catLifeStageFactor = 1.5
+        }
     } else {
-        catLifeStageFactor = 1.5
+        catLifeStageFactor = 1
+        catEnergyBase  = 418 - 104 * neuteredOrIndoor
     }
-} else {
-    catLifeStageFactor = 1
-    catEnergyBase  = 314 + 104 * neuteredOrIndoor
+    return catEnergyBase * weight**0.67 * catLifeStageFactor
 }
-//catWeight = catWeightLbs*0.453592
-const catDailyEnergy = catEnergyBase * catWeight**0.67 * lifeStageFactor
 
 //dogs
-
-if (dogAgeMonths > 12) {
-    const dogLifeStageFactor = [1.3, 1, 0.87]
-    const dogActivityBase = [398, 460, 523, 680, 4395]
-    const dogEnergyBase = dogActivityBase * dogLifeStageFactor
-    const dogDailyEnergy = dogEnergyBase * dogWeight**0.75
-} else if (dogAgeMonths > 2) {
-    const dogDailyEnergy = 1063 * ((dogWeight**1.75)/expectedDogWeight)
-} else {
-    const dogDailyEnergy = 1050 * dogWeight
+function calculateDogFeeding(ageMonths, activityLevel) {
+    if (ageMonths > 12) {
+        let dogLifeStageFactor
+        if (ageMonths < 36) {
+            dogLifeStageFactor = 1.3
+        } else if (ageMonths < 84) {
+            dogLifeStageFactor = 1
+        } else {
+            dogLifeStageFactor = 0.87
+        }
+        const dogActivityBase = [398, 460, 523, 680, 4395]
+        const dogEnergyBase = dogActivityBase[activityLevel] * dogLifeStageFactor
+        return dogEnergyBase * dogWeight**0.75
+    } else if (ageMonths > 2) {
+        return 1063 * ((dogWeight**1.75)/expectedDogWeight)
+    } else {
+        return 1050 * dogWeight
+    }
 }
