3594f232e29377b0b4537cb1f5b76e75b6bb0719
Ozan <me@maboxeel.com>
2025-04-09 08:58:41 +0200

    move images to the public folder

--- frontend/feed-this-much-client/src/app/home/page.js
+++ frontend/feed-this-much-client/src/app/home/page.js
@@ -12,8 +12,6 @@ import { Button } from "@/components/ui/button";
 
 import {redirect} from "next/navigation"
 
-import plus from "@/../../../docs/assets/img/plus-sign-circle-icon.png"
-
 export default function Home(){
     const pets = [{id:1, title: "Mac"}]; //will need to be fetched from API
     const foods = [{id:1, title: "Fancy Chow"}];
@@ -40,7 +38,7 @@ export default function Home(){
                             }
                             <div className="inline-flex items-center gap-2 !p-[2px]">
                                 <Button variant="plus" onClick={() => redirect("/add-pet")}>
-                                    <Image src={plus}
+                                    <Image src="/plus-sign-circle-icon.png"
                                         alt="" 
                                         width={20} 
                                         height={20} 
@@ -61,7 +59,7 @@ export default function Home(){
                             }
                             <div className="inline-flex items-center gap-2 !p-[2px]">
                                 <Button variant="plus">
-                                    <Image src={plus}
+                                    <Image src="/plus-sign-circle-icon.png"
                                         alt="" 
                                         width={20} 
                                         height={20} 
@@ -82,7 +80,7 @@ export default function Home(){
                             }
                             <div className="inline-flex items-center gap-2 !p-[2px]">
                                 <Button variant="plus">
-                                    <Image src={plus}
+                                    <Image src="/plus-sign-circle-icon.png"
                                         alt="" 
                                         width={20} 
                                         height={20} 
