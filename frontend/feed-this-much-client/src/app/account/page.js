5245d5a830fe1971db3ed7a425ee4a0091f03733
dnnrn <noren.danni@gmail.com>
2025-04-09 16:22:02 +0200

    new pages for profile

---
+++ frontend/feed-this-much-client/src/app/account/page.js
@@ -0,0 +1,14 @@
+"use client"
+
+export default function Account() {
+    return (
+        <div style={{ padding: "5%" }}>
+            <h1 className="scroll-m-20 text-2xl  text-[var(--custom-orange)] font-bold tracking-tight lg:text-5xl">
+                Account
+            </h1>
+            <p className="text-l leading-5 text-[var(--custom-orange)] [&:not(:first-child)]:mt-4">
+            Under construction.
+            </p>
+        </div>
+    )
+}
