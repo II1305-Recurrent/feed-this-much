61ff02b0c4384f8257dba0be106e88813ce42a88
Sajed <asmaz@kth.se>
2025-04-09 08:46:18 +0200

    Fixed test

--- backend/feed_this_much/pets/views.py
+++ backend/feed_this_much/pets/views.py
@@ -25,7 +25,7 @@ def get_pets(request):
     if not pets.exists():
         return Response(
             {"message": "No pets yet!"},
-            status=status.HTTP_200_OK
+            status=status.HTTP_200_OK #
         )
     serializer = PetSerializer(pets, many=True)  
     return Response(serializer.data, status=status.HTTP_200_OK)
