8f48217839c830eb992498ecd8acee18ddc2b3e0
Ozan <me@maboxeel.com>
2025-04-09 11:58:23 +0200

    fix

--- frontend/feed-this-much-client/src/app/add-food/page.js
+++ frontend/feed-this-much-client/src/app/add-food/page.js
@@ -3,7 +3,6 @@
 import {redirect} from "next/navigation"
 import { Button } from "@/components/ui/button"
 
-import back from "@/../../../docs/assets/img/back-arrow.png"
 import styles from "@/app/page.module.css";
 import Image from "next/image";
 
