1442580464d528d71324828512474223dc375c46
Ozan <me@maboxeel.com>
2025-04-09 16:56:25 +0200

    fix test db

--- backend/feed_this_much/settings.py
+++ backend/feed_this_much/settings.py
@@ -111,11 +111,10 @@ DATABASES = {
 		'NAME': os.getenv('POSTGRES_DB'),
 		'USER': os.getenv('POSTGRES_USER'),
 		'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-	},
-	'TEST': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': BASE_DIR / 'db.sqlite3',
-	}
+		'TEST': {
+			'ENGINE': 'django.db.backends.sqlite3',
+			'NAME': BASE_DIR / 'db.sqlite3',
+		}
 }
 
 if DEBUG:
