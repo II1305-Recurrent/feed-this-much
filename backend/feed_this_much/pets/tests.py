d66e3799b7fd353f2c768d146729b5ffda0e4e3a
Sajed <asmaz@kth.se>
2025-04-09 08:46:18 +0200

    Fixed test

--- backend/feed_this_much/pets/tests.py
+++ backend/feed_this_much/pets/tests.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
-from .models import Pet
 
 class PetAPITests(APITestCase):
     def setUp(self):
