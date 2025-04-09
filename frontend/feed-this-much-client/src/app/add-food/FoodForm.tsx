44be527d807c406869efd2795a8fb3d967545cbf
Alicia Emily Van Zijl <aevz@kth.se>
2025-04-09 11:02:25 +0200

    Create more inputs in the form

--- frontend/feed-this-much-client/src/app/add-food/FoodForm.tsx
+++ frontend/feed-this-much-client/src/app/add-food/FoodForm.tsx
@@ -36,6 +36,13 @@ function FoodForm() {
       resolver: zodResolver(addFoodSchema),
       defaultValues: {
         foodname: "",
+        foodServingType: undefined,
+        servingWeightAmount: "" as unknown as number,
+        servingWeightUnit: undefined,
+        energyAmount: "" as unknown as number,
+        energyUnit: undefined,
+        perWeightAmount: "" as unknown as number,
+        weightUnit: undefined,
       },
     })
    
@@ -64,6 +71,181 @@ function FoodForm() {
                     <FormMessage className={undefined} />
                 </FormItem>
               )}
+            />
+            <FormField
+            control={form.control}
+            name="foodServingType"
+            render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel className={undefined}>Food Servings</FormLabel>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex flex-col space-y-1"
+                >
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="tin" className={undefined} />
+                    </FormControl>
+                    <FormLabel className="font-normal">
+                      Tin
+                    </FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="sachet" className={undefined} />
+                    </FormControl>
+                    <FormLabel className="font-normal">
+                      Sachet
+                    </FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="carton" className={undefined} />
+                    </FormControl>
+                    <FormLabel className="font-normal">
+                      Carton
+                    </FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="scoop" className={undefined} />
+                    </FormControl>
+                    <FormLabel className="font-normal">
+                      Scoop (if you use a scoop or spoon to count servings)
+                    </FormLabel>
+                  </FormItem>
+                </RadioGroup>
+              </FormControl>
+              <FormMessage className={undefined} />
+            </FormItem>
+          )}
+          />
+          <FormField
+              control={form.control}
+              name="servingWeightAmount"
+              render={({ field }) => (
+                <FormItem className={undefined}>
+                  {/*<FormLabel className={undefined}>Energy Per Weight Amount</FormLabel>*/}
+                  <FormDescription className={undefined}>
+                    Now enter the weight of food in the select serving type. For example, if a scoop holds 25g of the food, enter 25 here and select grams below.
+                    </FormDescription>
+                  <FormControl>
+                    <Input placeholder="Enter weight number here" {...field} />
+                  </FormControl>
+                  <FormMessage className={undefined} />
+                </FormItem>
+              )}
+            />
+            <FormField
+            control={form.control}
+            name="servingWeightUnit"
+            render={({ field }) => (
+            <FormItem className={undefined}>
+              {/* <FormLabel className={undefined}>Select Unit</FormLabel> */}
+              <FormDescription className={undefined}>
+                Select the unit used for the serving weight amount you entered above.
+              </FormDescription>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger className={undefined} >
+                    <SelectValue placeholder="Select Weight Unit" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent className={undefined} >
+                  <SelectItem className={undefined} value="Kgs">Kgs</SelectItem>
+                  <SelectItem className={undefined} value="grams">grams</SelectItem>
+                  <SelectItem className={undefined} value="lbs">lbs</SelectItem>
+                  <SelectItem className={undefined} value="ounces">ounces</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage className={undefined} />
+            </FormItem>
+            )}
+            />
+            <FormField
+              control={form.control}
+              name="energyAmount"
+              render={({ field }) => (
+                <FormItem className={undefined}>
+                  <FormLabel className={undefined}>Metabolizable Energy (Energy per Weight)</FormLabel>
+                  <FormDescription className={undefined}>
+                    Enter the energy amount per weight. Look for something like &quot;360KJ/100g&quot; or &quot;62kcal/85g&quot;. Enter the number here and then select the unit below.
+                    </FormDescription>
+                  <FormControl>
+                    <Input placeholder="Enter energy number here" {...field} />
+                  </FormControl>
+                  <FormMessage className={undefined} />
+                </FormItem>
+              )}
+            />
+            <FormField
+            control={form.control}
+            name="energyUnit"
+            render={({ field }) => (
+            <FormItem className={undefined}>
+              {/* <FormLabel className={undefined}>Select Unit</FormLabel> */}
+              <FormDescription className={undefined}>
+                Select the unit used for the energy amount entered above.
+              </FormDescription>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger className={undefined} >
+                    <SelectValue placeholder="Select Energy Unit" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent className={undefined} >
+                  <SelectItem className={undefined} value="KJ">KJ</SelectItem>
+                  <SelectItem className={undefined} value="kcal">kcal</SelectItem>
+                  <SelectItem className={undefined} value="calories">calories</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage className={undefined} />
+            </FormItem>
+            )}
+            />
+            <FormField
+              control={form.control}
+              name="perWeightAmount"
+              render={({ field }) => (
+                <FormItem className={undefined}>
+                  {/*<FormLabel className={undefined}>Energy Per Weight Amount</FormLabel>*/}
+                  <FormDescription className={undefined}>
+                    Now enter the weight amount. So if the packet says &quot;360KJ/100g&quot;, enter 100 here and select grams below.
+                    </FormDescription>
+                  <FormControl>
+                    <Input placeholder="Enter weight number here" {...field} />
+                  </FormControl>
+                  <FormMessage className={undefined} />
+                </FormItem>
+              )}
+            />
+            <FormField
+            control={form.control}
+            name="weightUnit"
+            render={({ field }) => (
+            <FormItem className={undefined}>
+              {/* <FormLabel className={undefined}>Select Unit</FormLabel> */}
+              <FormDescription className={undefined}>
+                Select the unit used for the weight amount you entered above.
+              </FormDescription>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger className={undefined} >
+                    <SelectValue placeholder="Select Weight Unit" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent className={undefined} >
+                  <SelectItem className={undefined} value="Kgs">Kgs</SelectItem>
+                  <SelectItem className={undefined} value="grams">grams</SelectItem>
+                  <SelectItem className={undefined} value="lbs">lbs</SelectItem>
+                  <SelectItem className={undefined} value="ounces">ounces</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage className={undefined} />
+            </FormItem>
+            )}
             />
             <Button type="submit" className={undefined} variant={undefined} size={undefined}>Submit</Button>
           </form>
--- frontend/feed-this-much-client/src/zod-schemas/food.ts
+++ frontend/feed-this-much-client/src/zod-schemas/food.ts
@@ -5,8 +5,39 @@ import { z } from "zod";
 export const addFoodSchema = z.object({
 
     foodname: z.string().min(2, "Food name must be at least 2 characters."),
-
-
+    foodServingType: z.enum(["tin", "sachet", "carton", "scoop"], {
+        required_error: "You need to select a serving type.",
+      }),
+    servingWeightAmount: z.coerce
+      .number({
+        required_error: "A weight value is required",
+        invalid_type_error: "Weight amount must be a number",
+      })
+      .max(10000, "Weight must be no more than 10000. If your serving has a higher weight value than this...we have questions.")
+      .positive("Weight value must be greater than 0.  If your serving has no weight, you are starving your pet."),
+    servingWeightUnit: z.enum(["Kgs", "grams", "lbs", "ounces"], {
+          required_error: "Selecting a unit is required"
+    }),
+    energyAmount: z.coerce
+    .number({
+      required_error: "An energy value is required",
+      invalid_type_error: "Energy amount must be a number",
+    })
+    .max(10000, "Energy must be no more than 10000. If your food has a higher energy value than this...we have questions.")
+    .positive("Energy value must be greater than 0.  If your food has no energy, it is not complete pet food."),
+    energyUnit: z.enum(["KJ", "kcal", "calories"], {
+        required_error: "Selecting a unit is required"
+    }),
+    perWeightAmount: z.coerce
+    .number({
+      required_error: "A weight value is required",
+      invalid_type_error: "Weight amount must be a number",
+    })
+    .max(10000, "Weight must be no more than 10000. If your food has a higher weight value than this...we have questions.")
+    .positive("Weight value must be greater than 0.  If your food has no weight, it is not complete pet food."),
+    weightUnit: z.enum(["Kgs", "grams", "lbs", "ounces"], {
+        required_error: "Selecting a unit is required"
+    }),
 })
 
 export type addFoodSchemaType = typeof addFoodSchema._type
