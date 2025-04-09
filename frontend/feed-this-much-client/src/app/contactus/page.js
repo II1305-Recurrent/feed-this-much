149626f5c6b35db228524df1a82d31ca05bcac49
Alicia Emily Van Zijl <aevz@kth.se>
2025-04-09 15:03:16 +0200

    Create Contact Us and About Us Page

---
+++ frontend/feed-this-much-client/src/app/contactus/page.js
@@ -0,0 +1,11 @@
+"use client"
+
+export default function ContactUs() {
+    return (
+        <div style={{ padding: "5%" }}>
+            <h1 className="scroll-m-20 text-2xl  text-[var(--custom-orange)] font-bold tracking-tight lg:text-5xl">
+                Contact Us
+            </h1>
+        </div>
+    )
+}
