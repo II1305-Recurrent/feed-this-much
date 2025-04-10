216b8bf3d67df872b6aba65949d91ccabe5db212
Erik Sevelin <91209290+SortHvit@users.noreply.github.com>
2025-04-10 14:05:59 +0200

    fix merge mistakes

--- backend/feed_this_much/pets/views.py
+++ backend/feed_this_much/pets/views.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from feed_this_much.pets.models import Pet
-from feed_this_much.feeding.models import Food
 from .serializers import PetSerializer 
 
 @api_view(['POST'])
@@ -26,36 +25,3 @@ def get_pets(request):
             status=status.HTTP_200_OK # 200 OK even if no pets exist
         )
     return Response(status=status.HTTP_200_OK)
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def generate_plan(request):
-     error_message = ""
-     pet = Pet.objects.filter(user=request.user, name=request.petname) # Filter by userID, petname
-     food = Food.objects.filter(user=request.user, name=request.foodname) # Filter by userID, foodname
-     #energy_needs = None
- 
-     if not pet.exists():
-         error_message = "No such pet exists"
-         if not food.exists():
-             error_message += ", no such food exists"
-         return Response(
-             {"error": error_message},
-             status=status.HTTP_400_BAD_REQUEST
-         )
- 
-     if not food.exists():
-         error_message = "No such food exists"
-         return Response(
-             {"error": error_message},
-             status=status.HTTP_400_BAD_REQUEST
-         )
-     
-     #if pet.species == "cat":
-         #energy_needs = calorie_calculator.calculate_cat_feeding(pet)
-     #elif pet.species == "dog":
-         #energy_needs = calorie_calculator.calculate_dog_feeding(pet)
- 
-     # from the food, we get kJ per amount
-     # make energy_needs - food_energy = 0
-     # if overflow, limit to nearest decimal
