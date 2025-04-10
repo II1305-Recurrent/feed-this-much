c73c3bb9d323e0ac52294ee3103a0dd42448b5e5
Erik Sevelin <91209290+SortHvit@users.noreply.github.com>
2025-04-10 13:50:31 +0200

    minor edits to feeding

--- backend/feed_this_much/feeding/calorie_calculator.py
+++ backend/feed_this_much/feeding/calorie_calculator.py
@@ -1,9 +1,17 @@
-#body_weight: kg
-#daily_energy: kJ
+import datetime
 
-def calculate_cat_feeding(age_months, neutered_or_indoor, weight): #neuteredOrIndoor: 1 if true, 0 if false
-    energy_base = 0
-    life_stage_factor = 0
+#body_weight: kg
+# returns daily energy needs in kJ
+
+# TODO: get the right inputs
+def calculate_cat_feeding(pet):
+    weight = pet.weight
+    if pet.weight_unit == pet.WEIGHT_UNITS[1]:
+        weight *= 0.453592
+    energy_base = None
+    life_stage_factor = None
+    age_months = (datetime.date(pet.dob) - datetime.date.today()).days/12
+
     if (age_months < 12):
         energy_base  = 314
         if (age_months < 4):
@@ -14,19 +22,30 @@ def calculate_cat_feeding(age_months, neutered_or_indoor, weight): #neuteredOrIn
             life_stage_factor = 1.5
     else:
         life_stage_factor = 1
-        energy_base  = 418 - 104 * neutered_or_indoor
+        if pet.neutered:
+            energy_base  = 418 - 104
+        else:
+            energy_base  = 418
 
     return energy_base * weight**0.67 * life_stage_factor
 
-def calculate_dog_feeding(age_months, activity_level, weight, expected_weight):
+def calculate_dog_feeding(pet):
+    activity_level = None #fix this
+    expected_weight = None #fix tihs
+    weight = pet.weight
+    if pet.weight_unit == pet.WEIGHT_UNITS[1]:
+        weight *= 0.453592
+    age_months = (datetime.date(pet.dob) - datetime.date.today()).days/12
+
     if (age_months > 12):
-        life_stage_factor = 0
+        life_stage_factor = None
         if (age_months < 36):
             life_stage_factor = 1.3
         elif (age_months < 84):
             life_stage_factor = 1
         else:
             life_stage_factor = 0.87
+
         activity_base = [398, 460, 523, 680, 4395]
         energy_base = activity_base[activity_level] * life_stage_factor
         return energy_base * weight**0.75
