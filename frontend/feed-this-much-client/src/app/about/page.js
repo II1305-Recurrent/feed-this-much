be5045d8426712df31a0c6e7adcd8acf158ae71e
Alicia Emily Van Zijl <aevz@kth.se>
2025-04-10 09:26:40 +0200

    Fix unescaped character

--- frontend/feed-this-much-client/src/app/about/page.js
+++ frontend/feed-this-much-client/src/app/about/page.js
@@ -7,8 +7,8 @@ export default function About() {
                 About Us
             </h1>
             <p className="text-l leading-5 text-[var(--custom-orange)] [&:not(:first-child)]:mt-4">
-                We're a team of students from KTH working on a group project for the II1305 course.
-                Our goal is to build a web app that creates personalized feeding plans for pet owners, tailored to their pet’s unique needs and lifestyle based 
+                We&apos;re a team of students from KTH working on a group project for the II1305 course.
+                Our goal is to build a web app that creates personalized feeding plans for pet owners, tailored to their pet&apos;s unique needs and lifestyle based 
                 on the food information they provide.
             </p>
         </div>
