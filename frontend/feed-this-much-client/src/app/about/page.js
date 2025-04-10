41f22dae8d0c6d15772eee7abac4f755cd0d5ff1
dnnrn <noren.danni@gmail.com>
2025-04-10 09:15:17 +0200

    updated the cat and about page

--- frontend/feed-this-much-client/src/app/about/page.js
+++ frontend/feed-this-much-client/src/app/about/page.js
@@ -7,7 +7,9 @@ export default function About() {
                 About Us
             </h1>
             <p className="text-l leading-5 text-[var(--custom-orange)] [&:not(:first-child)]:mt-4">
-            Under construction.
+                We're a team of students from KTH working on a group project for the II1305 course.
+                Our goal is to build a web app that creates personalized feeding plans for pet owners, tailored to their pet’s unique needs and lifestyle based 
+                on the food information they provide.
             </p>
         </div>
     )
