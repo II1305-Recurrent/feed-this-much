00b271e68904d551633eb1481f0599d9bae3b48e
Kinga Koltai <koltai@kth.se>
2025-04-10 08:44:53 +0200

    Fixing zod form

--- frontend/feed-this-much-client/src/app/calc-new-plan/addPlanForm.tsx
+++ frontend/feed-this-much-client/src/app/calc-new-plan/addPlanForm.tsx
@@ -5,6 +5,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { z } from "zod"
 
 import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 
 import {
@@ -27,15 +28,18 @@ import {
 
 import { Input } from "@/components/ui/input"
 
-import { addPlanSchema, type addPlanSchemaType } from "@/zod-schemas/food"
+import { addPlanSchema, type addPlanSchemaType } from "@/zod-schemas/addPlan"
 
 function AddPlanForm() {
+    const pets = [{ id: 0, name: "Little bitch" }, { id: 1, name: "Poppy" }]
+    const foods = [{ id: 0, name: "Fancy chow" }, { id: 1, name: "Foooood" }]
 
     // 1. Define your form.
     const form = useForm<z.infer<typeof addPlanSchema>>({
         resolver: zodResolver(addPlanSchema),
         defaultValues: {
-            foodname: "Air",
+            title: "Default title", //maybe generate random number based on ids of previous plans?
+            foodname: [],
             petname: "Steve"
         },
     })
@@ -49,15 +53,15 @@ function AddPlanForm() {
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                 <FormField
                     control={form.control}
-                    name="foodname"
+                    name="title"
                     render={({ field }) => (
                         <FormItem className={undefined}>
                             <FormLabel className={undefined}>Pet Name</FormLabel>
                             <FormControl>
-                                <Input placeholder="Enter the name of the food" {...field} />
+                                <Input placeholder="Enter a name for the plan" {...field} />
                             </FormControl>
                             <FormDescription className={undefined}>
-                                Use a memorable name or nickname for the food.
+                                Use a memorable name for the plan.
                             </FormDescription>
                             <FormMessage className={undefined} />
                         </FormItem>
@@ -67,15 +71,66 @@ function AddPlanForm() {
                 />
                 <FormField
                     control={form.control}
+                    name="foodname"
+                    render={() => (
+                        <FormItem className={undefined}>
+                            <FormLabel className={undefined}>Food name(s)</FormLabel>
+                            {foods.map((item) => (
+                                <FormField
+                                    key={item.id}
+                                    control={form.control}
+                                    name="foodname"
+                                    render={({ field }) => {
+                                        return (
+                                            <FormItem
+                                                key={item.id}
+                                                className="flex flex-row items-start space-x-3 space-y-0"
+                                            >
+                                                <FormControl>
+                                                    <Checkbox
+                                                        className={undefined}
+                                                        checked={field.value?.includes(item.id)}
+                                                        onCheckedChange={(checked) => {
+                                                            return checked
+                                                                ? field.onChange([...field.value, item.id])
+                                                                : field.onChange(
+                                                                    field.value?.filter(
+                                                                        (value) => value !== item.id
+                                                                    )
+                                                                )
+                                                        }}
+                                                    />
+                                                </FormControl>
+                                                <FormLabel className="text-sm font-normal">
+                                                    {item.name}
+                                                </FormLabel>
+                                            </FormItem>
+                                        )
+                                    }}
+                                />
+                            ))}
+                            <FormDescription className={undefined}></FormDescription>
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
                     name="petname"
                     render={({ field }) => (
                         <FormItem className={undefined}>
-                            <FormLabel className={undefined}>Food Name</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Enter the name of the food" {...field} />
-                            </FormControl>
+                            <FormLabel className={undefined}>Pet Name</FormLabel>
+                            <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                <FormControl>
+                                    <SelectTrigger className={undefined} >
+                                        <SelectValue placeholder="Select pet" />
+                                    </SelectTrigger>
+                                </FormControl>
+                                <SelectContent className={undefined} >
+                                    {pets.map((item) => <SelectItem key={item.id} className={undefined} value={item.name}>{item.name}</SelectItem>)}
+                                </SelectContent>
+                            </Select>
                             <FormDescription className={undefined}>
-                             Choose name
+                                Please choose a pet
                             </FormDescription>
                             <FormMessage className={undefined} />
                         </FormItem>
