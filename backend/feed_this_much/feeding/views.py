97b413f9336d42886c30c268609b1baa324f712f
Erik Sevelin <91209290+SortHvit@users.noreply.github.com>
2025-04-10 13:50:31 +0200

    minor edits to feeding

--- backend/feed_this_much/feeding/views.py
+++ backend/feed_this_much/feeding/views.py
@@ -2,8 +2,9 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from feed_this_much.pets.models import Pet, Food
-from backend.feed_this_much.feeding import calorie_calculator
+from feed_this_much.pets.models import Pet
+from feed_this_much.feeding.models import Food
+#from backend.feed_this_much.feeding import calorie_calculator
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
@@ -11,6 +12,7 @@ def generate_plan(request):
     error_message = ""
     pet = Pet.objects.filter(user=request.user, name=request.petname) # Filter by userID, petname
     food = Food.objects.filter(user=request.user, name=request.foodname) # Filter by userID, foodname
+    #energy_needs = None
 
     if not pet.exists():
         error_message = "No such pet exists"
@@ -28,9 +30,15 @@ def generate_plan(request):
             status=status.HTTP_400_BAD_REQUEST
         )
     
-    if pet.species == "cat":
-        calorie_calculator.calculate_cat_feeding
-    elif pet.species == "dog":
-        calorie_calculator.calculate_dog_feeding
+    #if pet.species == "cat":
+        #energy_needs = calorie_calculator.calculate_cat_feeding(pet)
+    #elif pet.species == "dog":
+        #energy_needs = calorie_calculator.calculate_dog_feeding(pet)
     
+    # from the food, we get kJ per amount
+    # make energy_needs - food_energy = 0
+    # if overflow, limit to nearest decimal
+
+
+
     return Response(status=status.HTTP_200_OK)
