d037b52978b73efc66f8432875394e58e956cff6
Erik Sevelin <91209290+SortHvit@users.noreply.github.com>
2025-04-10 11:55:39 +0200

    add folder for feeding, transfer calculations, remove changes from pets folder

--- backend/feed_this_much/pets/models.py
+++ backend/feed_this_much/pets/models.py
@@ -27,17 +27,3 @@ class Pet(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.species})"
-
-class Food(models.Model):
-    WEIGHT_UNITS = [
-        ('kg', 'Kilograms'),
-        ('lb', 'Pounds'),
-    ]
-
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null = True)
-    name = models.CharField(max_length=255)
-    weight = models.FloatField()  
-    weight_unit = models.CharField(max_length=2, choices=WEIGHT_UNITS)  
-
-    def __str__(self):
-        return f"{self.name} ({self.weight})"
