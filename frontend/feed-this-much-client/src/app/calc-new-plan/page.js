5d92d568e7b99fd148ff23f9a6bdc0e85f5dfade
Kinga Koltai <koltai@kth.se>
2025-04-10 09:32:25 +0200

    Change pet name to id

--- frontend/feed-this-much-client/src/app/calc-new-plan/page.js
+++ frontend/feed-this-much-client/src/app/calc-new-plan/page.js
@@ -1,6 +1,6 @@
 "use client"
 
-import {redirect} from "next/navigation"
+import { redirect } from "next/navigation"
 import { Button } from "@/components/ui/button"
 
 import styles from "@/app/page.module.css";
@@ -9,21 +9,23 @@ import Image from "next/image";
 import AddPlanForm from "./addPlanForm"
 
 export default function foodPage() {
-    return (<div style={{padding: "5%"}}>
-        <h1 className="scroll-m-20 text-2xl  text-[var(--custom-orange)] font-bold tracking-tight lg:text-5xl">
-            Create new plan
-        </h1>
+    return (<div style={{ padding: "5%" }}>
+        <div className="flex justify-between">
+            <h1 className="scroll-m-20 text-2xl  text-[var(--custom-orange)] font-bold tracking-tight lg:text-5xl">
+                Add Food
+            </h1>
+            <Button variant="destructive" onClick={() => redirect("/home")}>
+                <Image src="/back-arrow.png"
+                    alt=""
+                    width={20}
+                    height={20}
+                    className={styles.logo}>
+                </Image>
+            </Button>
+        </div>
         <div className="flex justify-center min-h-screen">
-        <AddPlanForm />
-        <Button variant="destructive" onClick={() => redirect("/home")}>
-            <Image src="/back-arrow.png"
-                alt="" 
-                width={20} 
-                height={20} 
-                className={styles.logo}>
-            </Image>
-        </Button>
+            <AddPlanForm />
         </div>
-        </div>
+    </div>
     )
 }
