47a2b271cb6c7e65fb77e32f4f163f13ece0bc47
Kinga Koltai <koltai@kth.se>
2025-04-11 08:19:50 +0200

    Create context for default pet values

--- frontend/feed-this-much-client/src/app/Model.js
+++ frontend/feed-this-much-client/src/app/Model.js
@@ -11,8 +11,33 @@ export function ModelProvider({ children }) {
         return planIndex;
     };
     const setIndex = (value) => setPlanIndex(value)
+
+    const [cat, setCat] = useState({
+        name: "",
+        dob: "",
+        current_weight: "",
+        species: "cat",
+        neutered: undefined,
+        weight_unit: undefined,
+        condition_score: "3",
+        activity_level: undefined,
+    });
+    const setCatFields = ({ fieldName, value }) => setCat((prev) => ({ ...prev, [fieldName]: value }))
+
+    const [dog, setDog] = useState({
+        name: "",
+        dob: "",
+        current_weight: "",
+        expected_weight: "",
+        species: "dog",
+        neutered: undefined,
+        weight_unit: undefined,
+        condition_score: "3",
+        activity_level: undefined,
+    })
+    const setDogFields = ({ fieldName, value }) => setDog((prev) => ({ ...prev, [fieldName]: value }))
     return (
-        <ModelContext.Provider value={{ planIndex, getIndex, setIndex }}>
+        <ModelContext.Provider value={{ planIndex, getIndex, setIndex, cat, setCatFields, dog, setDogFields }}>
             {children}
         </ModelContext.Provider>
     );
