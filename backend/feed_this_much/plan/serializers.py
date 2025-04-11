adbd3e0071eb0d0bb5c4a85ee7939d00e467d314
Isabella Linnea LoPiano <lopiano@kth.se>
2025-04-11 13:26:14 +0200

    connected the generate plan to save in the database

---
+++ backend/feed_this_much/plan/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import UserPlan  
+
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserPlan
+        fields = ['user', 'pet', 'plan_title', 'pet_name', 'food_name', 'foor_serving_type', 'daily_energy_needs', 'daily_food_weight', 'daily_food_weight_unit', 'daily_servings_amount']
