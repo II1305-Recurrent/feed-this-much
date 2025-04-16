6e4dbb7e1f7db2648a76788e084b7ef5548cbbda
Kinga Koltai <koltai@kth.se>
2025-04-16 16:21:37 +0200

    First idea

--- frontend/feed-this-much-client/src/app/plans/page.js
+++ frontend/feed-this-much-client/src/app/plans/page.js
@@ -4,9 +4,22 @@ import { redirect } from "next/navigation"
 import { Button } from "@/components/ui/button"
 import { useModel } from "../Model";
 import Image from "next/image";
+import { useState } from "react";
+import { getRequest } from "@/utils/fetchApi";
 
 export default function Plan() {
-    const plan = { id: 0, title: "Plan 1 for Mac", pet: { id: 0, name: "Mac" }, foods: [{ id: 0, name: "Fancy Chow", amount: 2, servingType: "scoop" }, { id: 1, name: "Nice food", amount: 1, servingType: "tin" }] }
+    //const plan = { id: 0, title: "Plan 1 for Mac", pet: { id: 0, name: "Mac" }, foods: [{ id: 0, name: "Fancy Chow", amount: 2, servingType: "scoop" }, { id: 1, name: "Nice food", amount: 1, servingType: "tin" }] }
+    const [plans, setPlans] = useState([]);
+
+    async function getPlans() {
+        const response = await getRequest({ path: '/api/get-foods/' });
+        if (response.ok) {
+            if (response.payload !== "No plans yet") {
+                setPlans(response.payload);
+                console.log("Plans fetched")
+            }
+        }
+    }
     const { planIndex } = useModel();
     return (
         <div className="page">
@@ -20,7 +33,7 @@ export default function Plan() {
                         height={20} >
                     </Image>
                 </Button></div>
-            {plan.foods.map((item) =>
+            {plans[0].foods.map((item) =>
                 <div key={item.id} className="flex justify-between">
                     <p className="text-[var(--custom-brown)]">{item.name}</p>
                     <div className="flex gap-x-1"><p className="text-[var(--custom-brown)]">{item.amount}</p>
