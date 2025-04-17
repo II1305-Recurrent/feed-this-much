08ef762dde7c76fd493ddcb6c0e35a6fb938365f
Alicia Emily Van Zijl <aliciavanzijl@gmail.com>
2025-04-17 16:16:17 +0200

    Add plan id to serializer

--- backend/feed_this_much/plan/serializers.py
+++ backend/feed_this_much/plan/serializers.py
@@ -4,5 +4,5 @@ from .models import UserPlan
 class PlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserPlan
-        # TODO: ADD ID TO FIELDS
-        fields = ['user', 'pet', 'plan_title', 'food_name', 'food_serving_type', 'daily_energy_needs', 'daily_food_weight', 'daily_food_weight_unit', 'daily_servings_amount']
+        # TODO: ADD ID TO FIELDS - done
+        fields = ['id', 'user', 'pet', 'plan_title', 'food_name', 'food_serving_type', 'daily_energy_needs', 'daily_food_weight', 'daily_food_weight_unit', 'daily_servings_amount']
