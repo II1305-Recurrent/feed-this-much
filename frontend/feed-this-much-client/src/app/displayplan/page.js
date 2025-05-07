1fb59fa8db785ebea4d449e901b5788fbab2ac15
Ozan <me@maboxeel.com>
2025-05-07 14:58:44 +0200

    wrap DisplayPlan with Suspense

--- frontend/feed-this-much-client/src/app/displayplan/page.js
+++ frontend/feed-this-much-client/src/app/displayplan/page.js
@@ -2,6 +2,7 @@
 
 import { redirect } from "next/navigation"
 import { Button } from "@/components/ui/button"
+import { Suspense } from 'react';
 import Image from "next/image";
 
 import DisplayPlan from "./DisplayPlan";
@@ -10,17 +11,20 @@ export default function Plan() {
 
     return (
         <div className="page">
-            <div className="flex justify-between"><h1 className="scroll-m-20 text-2xl text-[var(--custom-orange)] font-bold tracking-tight lg:text-5xl !mb-3">
-                Feeding Plan
-            </h1>
-                <Button variant="destructive" onClick={() => redirect("/home")}>
-                    <Image src="/back-arrow.png"
-                        alt=""
-                        width={20}
-                        height={20} >
-                    </Image>
-                </Button></div>
-            <DisplayPlan />
+            <div className="flex justify-between">
+                <h1 className="scroll-m-20 text-2xl text-[var(--custom-orange)] font-bold tracking-tight lg:text-5xl !mb-3">
+                    Feeding Plan
+                </h1>
+            <Button variant="destructive" onClick={() => redirect("/home")}>
+                <Image src="/back-arrow.png"
+                    alt=""
+                    width={20}
+                    height={20} >
+                </Image>
+            </Button></div>
+            <Suspense>
+                <DisplayPlan />
+            </Suspense>
         </div>
     )
 
