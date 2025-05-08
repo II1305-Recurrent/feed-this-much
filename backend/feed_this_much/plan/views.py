0be6763a5f737c23c854c1a2d916756197178729
Erik Sevelin <91209290+SortHvit@users.noreply.github.com>
2025-05-08 15:17:30 +0200

    make notes for how to implement multiple foods in plan

--- backend/feed_this_much/plan/views.py
+++ backend/feed_this_much/plan/views.py
@@ -10,19 +10,13 @@ from .models import UserPlan
 
 @api_view(['POST', 'OPTIONS'])
 @permission_classes([IsAuthenticated])
-def generate_plan(request):
+def generate_plan(request): # GET AMOUNT OF FOODS (from food_id's), PERCENTAGES
     pet = Pet.objects.filter(user=request.user, id=request.data['pet_id']).first()
-    food = UserFood.objects.filter(user=request.user, id=request.data['food_id']).first() # Filter by userID, foodname
+    food = UserFood.objects.filter(user=request.user, id=request.data['food_id']).first() # Filter by userID, foodname CHANGE THIS. GET ALL REQUESTED FOODS!!!
     energy_needs = None
 
     if not pet:
         error_message = "No such pet exists"
-        return Response(
-            {"error": error_message},
-            status=status.HTTP_400_BAD_REQUEST
-        )
-    if not food:
-        error_message = "No such food exists"
         return Response(
             {"error": error_message},
             status=status.HTTP_400_BAD_REQUEST
@@ -33,6 +27,14 @@ def generate_plan(request):
     elif pet.species == "dog":
         energy_needs = calorie_calculator.calculate_dog_feeding(pet)
     
+    # FOR LOOP FOR FOODS
+    if not food:
+        error_message = "No such food exists"
+        return Response(
+            {"error": error_message},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
     # from the food, we get kJ per weight
 
     energy_given = food.energy
@@ -44,10 +46,11 @@ def generate_plan(request):
     # say we need 1000kJ, 1000 = (840*4.184)*portion_multiplier
     # portion_multiplier = 1000/(840*4.184)
 
+    ### MAKE THIS A FUNCTION, FOR EACH FOOD ENTER PERCENTAGE OF ENERGY NEEDS TO BE COVERED 
     food_to_packet_ratio = 0
     
 
-    portion_multiplier = energy_needs/energy_given # calculates how much of the food weight we need # THEN CALCULATE THIS WEIGHT DEPENDING ON OTHER WEIGHT, AND THAT WEIGHT'S FACTOR
+    portion_multiplier = energy_needs/energy_given # calculates how much of the food weight we need, calculate by percentage
     needed_food_weight = portion_multiplier*food.weight
     
     #weight_per_packet_unit MUST BE weight_unit for calculations to work
@@ -91,18 +94,20 @@ def generate_plan(request):
         elif food.weight_per_packet_unit == food.WEIGHT_UNITS[3][0]: #same
             food_to_packet_ratio = needed_food_weight/food.weight_per_packet
 
+    ###
+
     plan_data = {
         "user": request.user.id,
         "pet": pet.id,
-        "food": food.id,
+        "food": food.id, # UPDATE TO ARRAY OF FOODS
         "pet_name": pet.name,
-        "food_name": food.food_name,
+        "food_name": food.food_name, # UPDATE TO ARRAY OF FOODS
         "plan_title": request.data['plan_title'],
-        "food_serving_type": food.packet_type,
+        "food_serving_type": food.packet_type, # UPDATE TO ARRAY OF FOODS
         "daily_energy_needs": energy_needs,
-        "daily_food_weight": needed_food_weight,
-        "daily_food_weight_unit": food.weight_unit,
-        "daily_servings_amount": food_to_packet_ratio
+        "daily_food_weight": needed_food_weight, # UPDATE TO ARRAY OF FOODS
+        "daily_food_weight_unit": food.weight_unit, # UPDATE TO ARRAY OF FOODS
+        "daily_servings_amount": food_to_packet_ratio # UPDATE TO ARRAY OF FOODS
     }
 
     serializer = PlanSerializer(data=plan_data)
