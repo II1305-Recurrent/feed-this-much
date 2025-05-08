3ea21651f5df40f6b123d36ca6af3ffb7fd4754d
Ozan <me@maboxeel.com>
2025-05-08 13:46:11 +0200

    use localhost only

--- backend/feed_this_much/settings.py
+++ backend/feed_this_much/settings.py
@@ -137,8 +137,7 @@ if DEBUG:
     ]
     CORS_ALLOWED_ORIGINS = [
         "http://localhost:3000",
-        "http://localhost:8000",
-        "http://127.0.0.1:8000"
+        "http://localhost:8000"
     ]
     CSRF_COOKIE_DOMAIN = 'localhost'
 
