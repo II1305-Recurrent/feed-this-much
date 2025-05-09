26ce9e45437101de53878eaa763f3b18eb7c2739
Kinga Koltai <koltai@kth.se>
2025-05-09 14:00:35 +0200

    format links

--- frontend/feed-this-much-client/src/app/contact/page.js
+++ frontend/feed-this-much-client/src/app/contact/page.js
@@ -1,4 +1,4 @@
-
+import { Mail, Instagram } from "lucide-react"
 
 export default function Contact() {
     return (
@@ -6,23 +6,25 @@ export default function Contact() {
             <h1 className="scroll-m-20 text-2xl text-[var(--custom-orange)] font-bold tracking-tight lg:text-5xl !mb-3">
                 Contact Us
             </h1>
-            <div className="text-l leading-6 text-[var(--custom-orange)] !mb-4">
-                <p>
+            <div className="text-l leading-6 text-[var(--custom-orange)] !mb-4 !space-y-2">
+                <p className="flex gap-1 items-center">
+                    <Mail />
                     <strong>Email:</strong> {" "}
                     <a
                         href="mailto:feedthismuch@gmail.com"
-                        className="underline hover:text-[#4f759b]"
+                        className="!underline hover:text-[#4f759b]"
                     >
                         feedthismuch@gmail.com
                     </a>
                 </p>
-                <p>
+                <p className="flex gap-1 items-center">
+                    <Instagram />
                     <strong>Instagram:</strong>{" "}
                     <a
                         href="https://instagram.com/feedthismuch"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="underline hover:text-[#4f759b]"
+                        className="!underline hover:text-[#4f759b]"
                     >
                         @feedthismuch
                     </a>
